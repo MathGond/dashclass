7ad5e499a01344429585577424ba95168006dbf5
MathGond <matheus.gondim@gmail.com>
2025-04-10 18:38:31 -0400

    Update dashclass.py

--- dashclass.py
+++ dashclass.py
@@ -8,7 +8,7 @@ st.markdown("<h2 style='text-align: center;'>DashClass - Gerenciamento de Aulas<
 # Menu lateral
 st.sidebar.markdown("### Navegação")
 menu_opcoes = [
-    "Cadastro de Turmas",
+    "Cadastro de Turmas" (Tela inicial),
     "Registro de Aulas",
     "Controle de Aulas Dadas",
     "Gráfico de Aulas Dadas",
